92ead7914de4457920a5b3f6389dac88f781faa5
Andy Lien <andy922200@gmail.com>
2022-02-19 02:04:09 +0800

    1. init todoListRestful api

---
+++ todoListRestfulAPI/server.js
@@ -0,0 +1,67 @@
+const http = require('http')
+const port = 8080
+const requestListener = (req, res) =>{
+    const resHeaders = {
+        "Access-Control-Allow-Headers": 'Content-Type, Authorization, Content-Length, X-Requested-With',
+        "Access-Control-Allow-Origin": '*',
+        "Access-Control-Allow-Methods": 'PATCH, POST, GET, OPTIONS, DELETE',
+        "content-type": "application/json"
+    }
+
+    if(req.url === "/"){
+        switch (req.method){
+            case 'GET':
+                resContentGenerator({
+                    res,
+                    statusCode: 200,
+                    resHeaders,
+                    data: {
+                        status: 'success',
+                        message: 'Get your Index page'
+                    }
+                })
+                break
+            case 'DELETE':
+                resContentGenerator({
+                    res,
+                    statusCode: 200,
+                    resHeaders,
+                    data: {
+                        status: 'success',
+                        message: 'Delete successfully'
+                    }
+                })
+                break
+            default:
+                resContentGenerator({
+                    res,
+                    statusCode: 200,
+                    resHeaders,
+                    data: {
+                        status: 'success',
+                        message: 'Nothing happened'
+                    }
+                })
+                break
+        }
+        return;
+    }
+
+    resContentGenerator({
+        res,
+        statusCode: 404,
+        resHeaders,
+        data: {
+            status: false,
+            message: 'invalid route'
+        }
+    })
+}
+const resContentGenerator = ({ res, statusCode, resHeaders, data})=>{
+    res.writeHead(statusCode, resHeaders)
+    res.write(data ? JSON.stringify(data) : JSON.stringify({status: false, message: 'No Value'}))
+    res.end()
+}
+
+const httpServer = http.createServer(requestListener)
+httpServer.listen(port ? port : 8080)
