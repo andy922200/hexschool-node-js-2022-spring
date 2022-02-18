df4be66bd02f4ac7d319187bb5a2fc334982a399
andy lien <andy922200@gmail.com>
2022-02-18 13:36:29 +0800

    1. init project

---
+++ beforeStart/app.js
@@ -0,0 +1,15 @@
+/* open default web server */
+const http = require('http')
+const port = 8080
+
+const server = http.createServer(function(req,res){
+    res.writeHead(200, {"Content-Type":"text/plain"})
+    res.write('hello!!')
+    res.end()
+})
+
+server.listen(port ? port : 8080)
+
+// 分析路徑
+const path = require('path')
+console.log(path.parse('/xx/yy/zz.js'))
